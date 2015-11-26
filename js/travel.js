cb532362c954836e4255387810ab8f05aa692e96
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2015-11-26 02:12:00 -0500

    modal with form for search on button click, date picker almost works but position restricts it from expanding. working on fixing that

--- js/travel.js
+++ js/travel.js
@@ -1,4 +1,46 @@
-$( document ).ready(function() {
-$(window).load(function(){
-        $('#myModal').modal('show');
-    });
+$(document).ready(function() {
+  $("#myModal").on("hide.bs.modal", function() {
+    $("[data-target='#myModal']").fadeTo("slow", 0.25);
+  })
+ //$(".well-searchbox").addClass("picker");//
+//$("#checkinPicker").addClass("picker");
+// })
+
+var from_$input = $('#input_from').pickadate({
+  containerHidden: '#myModal'
+})
+    from_picker = from_$input.pickadate('picker')
+
+var to_$input = $('#input_to').pickadate({
+  containerHidden: '#myModal'
+})
+    to_picker = to_$input.pickadate('picker')
+
+
+// Check if there’s a “from” or “to” date to start with.
+if ( from_picker.get('value') ) {
+  to_picker.set('min', from_picker.get('select'))
+}
+if ( to_picker.get('value') ) {
+  from_picker.set('max', to_picker.get('select'))
+}
+
+// When something is selected, update the “from” and “to” limits.
+from_picker.on('set', function(event) {
+  if ( event.select ) {
+    to_picker.set('min', from_picker.get('select'))    
+  }
+  else if ( 'clear' in event ) {
+    to_picker.set('min', false)
+  }
+})
+to_picker.on('set', function(event) {
+  if ( event.select ) {
+    from_picker.set('max', to_picker.get('select'))
+  }
+  else if ( 'clear' in event ) {
+    from_picker.set('max', false)
+  }
+})
+});
+
